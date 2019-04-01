ffb9d1fde2dc536b3bca8301aeea3d59da4bfce6
Dmitry Khaleev <dkhaleev@gmail.com>
2019-04-01 22:24:45 +0300

    doubled steps per unit  for DRB8825

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -614,7 +614,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+ //8825 doubles steps
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 186 }
 
 /**
  * Default Max Feed Rate (mm/s)
